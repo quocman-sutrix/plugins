b8e69ed151a10dbbc0b98658dc7e86f5514e4b92
Quoc Man <man.lq@sutrixsolutions.com>
2020-08-04 10:50:13 +0700

    Update FLTSharePlugin.m

--- packages/share/ios/Classes/FLTSharePlugin.m
+++ packages/share/ios/Classes/FLTSharePlugin.m
@@ -105,6 +105,7 @@ static NSString *const PLATFORM_CHANNEL = @"plugins.flutter.io/share";
   activityViewController.popoverPresentationController.sourceView = controller.view;
   if (!CGRectIsEmpty(origin)) {
     activityViewController.popoverPresentationController.sourceRect = origin;
+    
   }
   [controller presentViewController:activityViewController animated:YES completion:nil];
 }
